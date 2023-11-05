c7605b73089e7bdf2bb15b634ee7190dc073f6d5
Stuart Mesham <stumes3@gmail.com>
2023-11-05 19:14:06 +0200

    minor refactor greedy_decode_without_cache.py

--- scripts/greedy_decode_without_cache.py
+++ scripts/greedy_decode_without_cache.py
@@ -2,7 +2,6 @@ from orbax.checkpoint import PyTreeCheckpointer
 from transformer import Transformer
 import jax.numpy as jnp
 import tensorflow_text as tf_text
-import tensorflow as tf
 
 checkpointer = PyTreeCheckpointer()
 restored_params = checkpointer.restore("model_saves/70/default")
@@ -13,20 +12,22 @@ transformer = Transformer(max_length=522, vocab_size=10149, emb_size=512, mlp_hi
 with open("model_saves/70/en-de.model", "rb") as f:
     tokenizer = tf_text.SentencepieceTokenizer(f.read(), add_eos=True)
 
-input = tokenizer.tokenize("i would really like a cup of tea.")
-input = tf.expand_dims(input, axis=0)
+token_ids = tokenizer.tokenize(["i would really like a cup of tea."]).numpy()
 
 batch = {
-    "inputs_ids": input.numpy(),
-    "bidirectional_attention_mask": tf.ones_like(input).numpy(),
+    "token_ids": token_ids,
+    "position_ids": jnp.broadcast_to(jnp.arange(0, token_ids.shape[1]), token_ids.shape),
+    "bidirectional_attention_mask": jnp.ones_like(token_ids),
 }
 
 while True:
     logits = transformer.apply({"params": restored_params}, batch, eval_mode=True)
+    token_ids = jnp.hstack((batch["token_ids"], logits.argmax(axis=2)[0, -1].reshape(1, 1)))
     batch = {
-        "inputs_ids": jnp.hstack((batch["inputs_ids"], logits.argmax(axis=2)[0, -1].reshape(1, 1))),
+        "token_ids": token_ids,
+        "position_ids": jnp.broadcast_to(jnp.arange(0, token_ids.shape[1]), token_ids.shape),
         "bidirectional_attention_mask": jnp.hstack((batch["bidirectional_attention_mask"], jnp.zeros((1, 1), "int32")))
     }
-    if batch["inputs_ids"][0, -1] == 2:  # if model outputs EOS token
+    if batch["token_ids"][0, -1] == 2:  # if model outputs EOS token
         break
-    print(tokenizer.detokenize(batch["inputs_ids"]).numpy()[0].decode())
+    print(tokenizer.detokenize(batch["token_ids"]).numpy()[0].decode())
